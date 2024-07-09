e8cecba81a0197fc49e0bb77bd9f85b2474aff0f
ZenDev <76600019+ZenCreates@users.noreply.github.com>
2024-07-08 17:51:28 -0700

    Create execution.lua

---
+++ execution.lua
@@ -0,0 +1,5 @@
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ZenCreates/ZensDoorsDevTools/main/skibidi.lua"))()
+end
+-- extra scripts
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
